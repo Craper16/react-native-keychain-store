ccb79dbffc736105299c42b1ce86bdf9f2c9e13c
Craper16 <georgio.saad@gmail.com>
2025-04-02 01:31:25 +0300

    feat: adding non default exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,19 @@
 import KeychainStore from './NativeKeychainStore';
 
+export function getItem(key: string) {
+  return KeychainStore.getItem(key);
+}
+
+export function removeItem(key: string) {
+  return KeychainStore.removeItem(key);
+}
+
+export function clear() {
+  return KeychainStore.clear();
+}
+
+export function setItem(key: string, value: string) {
+  return KeychainStore.setItem(key, value);
+}
+
 export default KeychainStore;
